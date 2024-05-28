d32602c2055e5b42b6fb6eb7aa6f7726f9467209
Larissa Geovana Ferreira Zenaro <00001095744094sp@al.educacao.sp.gov.br>
2024-05-28 18:03:41 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
-    background(220);
-  }
+  createCanvas(400, 400); 
+  background("white");
+}
+
+function draw() {
+  stroke ("black") 
+fill("green")
+
+//console.log(moauseIsPressed); 
+    if (mouseIsPressed){  
+      rect (mouseX, mouseY, 20, 35);
+            }
+}
